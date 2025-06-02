0737449b8b14d85cabad52b0067771071742e892
Tiago Caldas <tiago.rodrigues.caldas@gmail.com>
2025-06-02 20:52:29 +0100

    fix: enforce minimum name length of 3 characters on item creation and update

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class Item(BaseModel):
 
 
 class ItemCreate(BaseModel):
-    name: str = Field(...)
+    name: str = Field(min_length=3, description="Name must be at least 3 characters long")
     price: float
 
 
